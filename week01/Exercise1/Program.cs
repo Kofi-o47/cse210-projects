a506c7e02a85bb0724602c2ab639491be7e884aa
kofi-o47 <kofi.o47@yahoo.com>
2025-03-04 20:35:01 +0000

    user-input name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+
+        Console.Write("what is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+
     }
 }
