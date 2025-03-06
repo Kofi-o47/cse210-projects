968344ed60d94066be7018fd7066ed4977098fd5
kofi-o47 <kofi.o47@yahoo.com>
2025-03-06 17:52:59 +0000

    guess number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,101);
+
+        string keep_playing = "Yes";
+
+        while(keep_playing =="Yes")
+        {
+        int guess = -1;
+        int guess_count = 0;
+        
+            while(guess != magic_number)
+            {
+             Console.Write("What is your guess? ");
+             guess = int.Parse(Console.ReadLine());
+             guess_count = guess_count + 1;
+            
+
+             if(guess < magic_number)
+             {
+                Console.WriteLine("Higher");
+             }
+             else if(guess > magic_number)
+             {
+                Console.WriteLine("Lower");
+             }
+             else
+             {
+                Console.WriteLine("You guessed it!");
+             }
+
+            }
+        Console.WriteLine($"It took you {guess_count} guesses.");
+        Console.Write("Would you like to play again (Yes / No)? ");
+        keep_playing = Console.ReadLine();
+        
+        }
+
+        Console.WriteLine("Thank you for playing.Goodbye");
     }
 }
