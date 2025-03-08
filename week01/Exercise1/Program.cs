e1713973805a30ed72d9bf636a1c5fae028b9c34
kofi-o47 <kofi.o47@yahoo.com>
2025-03-08 20:58:51 +0000

    user.input.final

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,13 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // user input for first name 
         Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
 
-
+       // user input for last name 
         Console.Write("what is your last name? ");
         string lastname = Console.ReadLine();
-
+        
+        // display for first and last name 
         Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
 
     }
